42e9ae38d8eb3fd4aa6825e00c759529ad750aa7
RaissaKestring <raissakestring23@gmail.com>
2024-07-23 17:33:04 -0300

    Deletando colaborador

--- src/App.js
+++ src/App.js
@@ -138,7 +138,6 @@ function App() {
       time: times[3].nome
     },
     {
-      id: uuidv4(),
       nome: 'DANIEL ARTINE',
       cargo: 'Engenheiro de Software na Stone Age',
       imagem: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
@@ -218,11 +217,11 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState(inicial)
 
-  function deletarColaborador () {
-    console.log('deletando colaborador')
+  function deletarColaborador(id) {
+    setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id));
   }
 
-  function mudarCorDoTime(cor, id) {
+  function mudarCor(cor, id) {
     setTimes(times.map(time => {
       if(time.id === id) {
         time.cor = cor;
@@ -231,21 +230,18 @@ function App() {
     }));
   }
 
+  function cadastrarTime({ nome, cor }) {
+    setTimes([...times, { nome, cor, id: uuidv4() }])
+  }
+
 
   return (
     <div>
       <Banner />
-      <Formulario times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
+      <Formulario aoCriarTime={cadastrarTime} times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
       <section className="times">
         <h1>Minha organização</h1>
-          {times.map((time, indice) =>
-          <Time 
-              mudarCor={mudarCorDoTime}
-              key={indice} 
-              time={time} 
-              colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} 
-            />
-          )}
+        {times.map((time, indice) => <Time mudarCor={mudarCor} key={indice} time={time} colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} aoDeletar={deletarColaborador} />)}
       </section>
       <Rodape />
     </div>
